4c399c1a1ed3e097de93c94e49232ee8b65919c7
Johan Haals <johan.haals@gmail.com>
2015-12-02 09:10:29 +0100

    API cleanups, more docs

--- gohealthy.go
+++ gohealthy.go
@@ -1,3 +1,44 @@
+/*
+Package gohealthy allow you to register health checks that can be hooked up to your load balancer such as haproxy
+
+Example
+
+ package main
+
+ import (
+ 	"github.com/jhaals/gohealthy"
+ 	"time"
+ )
+
+ type customerPortal struct{}
+
+ func (f customerPortal) GetHealth() gohealthy.HealthStatus {
+ 	return gohealthy.HealthStatus{Name: "customer-satisfaction",
+ 		Message: "Customer Satisfaction is above 180%",
+ 		Healthy: true}
+ }
+
+ type mySQLMonitor struct{}
+
+ func (b mySQLMonitor) GetHealth() gohealthy.HealthStatus {
+ 	hs := gohealthy.HealthStatus{Name: "mysql-monitor"}
+ 	if time.Now().Weekday() == time.Friday {
+ 		hs.Message = "this is clearly not alright"
+ 		hs.Healthy = false
+ 	}
+ 	hs.Message = "its not Friday so everything is awesome"
+ 	hs.Healthy = true
+ 	return hs
+ }
+
+ func main() {
+ 	g := gohealthy.Init()
+ 	g.Register(customerPortal{})
+ 	g.Register(mySQLMonitor{})
+ 	g.RunServer(1337)
+ }
+
+*/
 package gohealthy
 
 import (
@@ -16,7 +57,7 @@ type HealthStatus struct {
 
 // HealthCheck Status() need to be implemented by services that want to display health
 type HealthCheck interface {
-	Status() HealthStatus
+	GetHealth() HealthStatus
 }
 
 // HealthChecks contains list of HealthChecks
@@ -24,29 +65,49 @@ type HealthChecks struct {
 	HealthChecks []HealthCheck
 }
 
+// Init a new instance of goheatlhy
+func Init() HealthChecks {
+	return HealthChecks{}
+}
+
 // Register new healthCheck
 func (hcs *HealthChecks) Register(healthCheck HealthCheck) {
 	hcs.HealthChecks = append(hcs.HealthChecks, healthCheck)
 }
 
-// Status of all healthChecks
-func (hcs *HealthChecks) Status() (map[string]HealthStatus, bool) {
+// RunServer starts a webserver serving health check status on specified port
+func (hcs *HealthChecks) RunServer(port int) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		hcs.HealthCheckHandler(w, r)
+	})
+	log.Println(fmt.Sprintf("Serving health checks on port %v", port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+/*
+GetHealthChecks returns a map with all registered health checks and a bool
+which will be false if one or more of the health checks return false
+*/
+func (hcs *HealthChecks) GetHealthChecks() (map[string]HealthStatus, bool) {
 	var m = make(map[string]HealthStatus)
 	heatlhy := true
 	for _, hc := range hcs.HealthChecks {
-		if !hc.Status().Healthy {
+		healthCheck := hc.GetHealth()
+		if !healthCheck.Healthy {
 			heatlhy = false
 		}
-		s := hc.Status()
-		m[s.Name] = s
+		m[healthCheck.Name] = healthCheck
 	}
 	return m, heatlhy
 }
 
-// StatusHandler to be used in webserver
-func (hcs *HealthChecks) StatusHandler(response http.ResponseWriter, request *http.Request) {
-	status, healthy := hcs.Status()
-	result, err := json.Marshal(status)
+/*
+HealthCheckHandler returns a json with all registered healthChecks.
+Response code will be set to 500 if one or more of the health checks are unhealthy
+*/
+func (hcs *HealthChecks) HealthCheckHandler(response http.ResponseWriter, request *http.Request) {
+	healthChecks, healthy := hcs.GetHealthChecks()
+	result, err := json.Marshal(healthChecks)
 	if err != nil {
 		http.Error(response, "Failed to json encode health status", 500)
 	}
@@ -55,18 +116,4 @@ func (hcs *HealthChecks) StatusHandler(response http.ResponseWriter, request *ht
 		return
 	}
 	response.Write(result)
-}
-
-// Init a new instance of goheatlhy
-func Init() HealthChecks {
-	return HealthChecks{}
-}
-
-// RunServer starts a webserver serving healthcheck status on specified port
-func (hcs *HealthChecks) RunServer(port int) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		hcs.StatusHandler(w, r)
-	})
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
--- gohealthy_test.go
+++ gohealthy_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 type mock struct{}
 
-func (m mock) Status() HealthStatus {
+func (m mock) GetHealth() HealthStatus {
 	return HealthStatus{"TestRegister", "This is just a test", true}
 }
 func TestRegister(t *testing.T) {
@@ -12,11 +12,11 @@ func TestRegister(t *testing.T) {
 	goheatlhy := HealthChecks{}
 	goheatlhy.Register(mock{})
 
-	result, status := goheatlhy.Status()
+	healthChecks, status := goheatlhy.GetHealthChecks()
 	if !status {
 		t.Errorf("this should be heatlhy")
 	}
-	if result["TestRegister"].Message != "This is just a test" {
+	if healthChecks["TestRegister"].Message != "This is just a test" {
 		t.Errorf("Failed to register and get healthcheck status")
 	}
 }
