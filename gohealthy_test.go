5815a46924c6812a6ede98ec3ff5310e4f1d5de7
Johan Haals <johan.haals@gmail.com>
2015-12-02 10:43:52 +0100

    Test HealthCheckHandler

--- gohealthy_test.go
+++ gohealthy_test.go
@@ -1,6 +1,11 @@
 package gohealthy
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 type mock struct{}
 
@@ -18,5 +23,39 @@ func TestRegister(t *testing.T) {
 	}
 	if healthChecks["TestRegister"].Message != "This is just a test" {
 		t.Errorf("Failed to register and get healthcheck status")
+	}
+}
+func TestHealthCheckHandlerHealthy(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	gohealthy := Init()
+	gohealthy.Register(mock{})
+	gohealthy.HealthCheckHandler(response, request)
+	expected := `This is just a test`
+	if !strings.Contains(response.Body.String(), expected) {
+		t.Errorf("Reponse body is %s, expected %s", response.Body, expected)
+	}
+	if response.Code != http.StatusOK {
+		t.Errorf("Response code is %v, should be 200", response.Code)
+	}
+}
+
+type mockUnhealthy struct{}
+
+func (m mockUnhealthy) GetHealth() HealthStatus {
+	return HealthStatus{"TestRegister", "This is just a test", false}
+}
+func TestHealthCheckHandlerUnhealthy(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	gohealthy := Init()
+	gohealthy.Register(mockUnhealthy{})
+	gohealthy.HealthCheckHandler(response, request)
+	expected := `This is just a test`
+	if !strings.Contains(response.Body.String(), expected) {
+		t.Errorf("Reponse body is %s, expected %s", response.Body, expected)
+	}
+	if response.Code != http.StatusInternalServerError {
+		t.Errorf("Response code is %v, should be 500", response.Code)
 	}
 }
